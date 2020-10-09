9ce8d3611c6963b29fdf8e1c5df2c0765d8c20a4
Nolan <60010605+nolngo@users.noreply.github.com>
2020-10-08 18:26:50 -0700

    fixed all issues javascript

---
+++ Resources/script.js
@@ -0,0 +1,18 @@
+var conversionRates = {
+    "jpy": 106.03,
+    "euro": 0.85,
+    "cny": 6.79,
+    "cad": 1.32
+}
+let button = document.getElementById("button");
+button.addEventListener("click",function(){
+    let foreignCurrency = document.getElementById("foreign").value;
+    let conversionRate = conversionRates[foreignCurrency];
+    let currencyAmount = document.getElementById("amount").value;
+    var toFrom = document.getElementById("to-from").value;
+        if (toFrom == "from-USD") {
+            let outputValue = document.getElementById("output").value = currencyAmount * conversionRate;
+        } else {
+            let outputValue = document.getElementById("output").value = currencyAmount / conversionRate;
+        }
+})
